7924e9c42094e71f50ffe95a124bac121e5cf88b
Simon <xg5-simon@users.noreply.github.com>
2018-12-21 17:21:22 +1100

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+CrowdStrike = {
+    'proxies':{
+        'http': '',
+        'https': ''
+    },
+    'url':'https://falconapi.crowdstrike.com',
+    'cs_user':'<YOUR_CROWDSTRIKE_QUERYAPI_KEY>',
+    'cs_key':'<YOUR_CROWDSTRIKE_QUERY_SECRET>',
+}
+
+TheHive = {
+    'proxies':{
+        'http': '',
+        'https': ''
+    },
+    'url':'<YOUR_THEHIVE_URL>',
+    'key':'<YOUR_THEHIVE_API_KEY>',
+    'template':''
+
+}
